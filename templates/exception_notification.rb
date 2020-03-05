88de4d98302ccad68f384d680601f1f9e674d597
labocho <labocho@penguinlab.jp>
2020-03-05 14:34:13 +0900

    update template

--- templates/exception_notification.rb
+++ templates/exception_notification.rb
@@ -1,6 +1,10 @@
-Rails.application.config.middleware.use(
-  ExceptionNotifier,
-  :email_prefix => "[Application name] ",
-  :sender_address => %{"Sender name" <sender-address>},
-  :exception_recipients => %w{recipient-addresses}
-)
+unless Rails.env == "development"
+  Rails.application.config.middleware.use(
+    ExceptionNotification::Rack,
+    email: {
+      email_prefix: "[Application name] ",
+      sender_address: %{"Sender name" <sender-address>},
+      exception_recipients: %w{recipient-addresses},
+    },
+  )
+end
