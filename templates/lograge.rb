705cccb0a4fd27a15c30632d157739f5b86fc42f
labocho <labocho@penguinlab.jp>
2024-07-05 09:47:29 +0900

    update lograge config

--- templates/lograge.rb
+++ templates/lograge.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_options = -> (event) {
-    time = (Process.clock_gettime(Process::CLOCK_MONOTONIC) - event.time).seconds.ago
+    time = (Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond) - event.time).seconds.ago
 
     params = event.payload[:params].reject do |k|
       %w(controller action).include? k
